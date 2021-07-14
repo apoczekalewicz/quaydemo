19680934414989152a1ffec02098f0753ebda458
ap <ap@laptop.ap.local>
2021-07-14 13:49:14 +0200

    add quay.sh

---
+++ quay.sh
@@ -0,0 +1,13 @@
+podman pull registry.access.redhat.com/ubi8/httpd-24:latest
+podman pull docker.io/library/httpd:latest
+
+
+podman tag docker.io/library/httpd:latest 			quay.io/quaydemolab/httpd-docker
+podman tag registry.access.redhat.com/ubi8/httpd-24:latest	quay.io/quaydemolab/httpd-rh
+
+podman login quay.io
+
+podman push quay.io/quaydemolab/httpd-docker
+podman push quay.io/quaydemolab/httpd-rh
+
+
